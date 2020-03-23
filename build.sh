468d9635e276133cfd99c15d1f84aa575e55ea18
Jay Colson <jay.colson@synchronoss.com>
2020-03-23 12:36:27 +0000

    added another dataset

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+DATASET="geocombined.json"
 PREPEND="package main
 
 // GEODATA is the default geojson data built into the binary
 const GEODATA string = \`"
 POSTPEND="\`"
-cat <(echo "${PREPEND}") geocombined.json <(echo "${POSTPEND}") > geojson.go
+cat <(echo "${PREPEND}") ${DATASET} <(echo "${POSTPEND}") > geojson.go
 
 go get ./...
 if [[ "${GOOS}" == "" ]] && [[ "${GOARCH}" == "" ]]; then
